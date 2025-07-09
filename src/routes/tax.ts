9570763cdd1cb62665ab4f6d2f24e60b70a6192c
GhazanSubz <gsubzwari@gmail.com>
2025-07-09 00:52:26 -0700

    added zod middleware and zod validators for all update routes

--- src/routes/tax.ts
+++ src/routes/tax.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { getAllTaxSlabsHandler, updateTaxSlabHandler } from '../controllers/taxController';
+import { validateBody } from '../middleware/validate';
+import { taxSchema } from '../validators/taxValidator';
 
 const router = express.Router();
 
 router.get('/', getAllTaxSlabsHandler);
 
-router.put('/:id', updateTaxSlabHandler);
+router.put('/:id', validateBody(taxSchema), updateTaxSlabHandler);
 
 export default router;
